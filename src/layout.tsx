a63e741eb07144a4820ffd745decaf6b3ac11a7d
Nguyên Vũ <fish17tb@gmail.com>
2025-03-02 23:58:36 +0700

    Handle-Refresh-Page-And-Loading-Page
    handle refresh page without loss data pass function fetchAccount  then allow access authorization pass bearer token and finally handle loading page

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,47 @@
 import { Outlet } from "react-router-dom";
-import AppHeader from "./components/layout/app.header";
+import AppHeader from "components/layout/app.header";
+import { useEffect } from "react";
+import { fetchAccountAPI } from "services/api";
+import { useCurrentApp } from "components/context/app.context";
+import { BounceLoader } from "react-spinners";
 
 const Layout = () => {
+  const { setIsAuthenticated, setUser, isAppLoading, setIsAppLoading } =
+    useCurrentApp();
+
+  useEffect(() => {
+    const fetchAccount = async () => {
+      const res = await fetchAccountAPI();
+      // console.log("ck-fetchAccount", res);
+      if (res && res.data) {
+        setIsAuthenticated(true);
+        setUser(res.data.user);
+      }
+      setIsAppLoading(false);
+    };
+
+    fetchAccount();
+  }, []);
+
   return (
     <>
-      <AppHeader />
-      <Outlet />
+      {isAppLoading === false ? (
+        <div>
+          <AppHeader />
+          <Outlet />
+        </div>
+      ) : (
+        <div
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <BounceLoader size={60} color="#0652DD" />
+        </div>
+      )}
     </>
   );
 };
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,15 @@ import axios from "./axios.customize";
 
 export const loginAPI = (username: string, password: string) => {
   const urlBackend = `/api/v1/auth/login`;
-  return axios.post<IBackendRes<ILogin>>(urlBackend, { username, password });
+  return axios.post<IBackendRes<ILogin>>(
+    urlBackend,
+    { username, password },
+    {
+      headers: {
+        delay: 1000,
+      },
+    }
+  );
 };
 
 export const registerAPI = (
@@ -17,5 +25,14 @@ export const registerAPI = (
     email,
     password,
     phone,
+  });
+};
+
+export const fetchAccountAPI = () => {
+  const urlBackend = `/api/v1/auth/account`;
+  return axios.get<IBackendRes<IFetchAccount>>(urlBackend, {
+    headers: {
+      delay: 3000,
+    },
   });
 };

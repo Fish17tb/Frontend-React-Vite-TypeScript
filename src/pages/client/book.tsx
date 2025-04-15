62c9886d6eea9eb6c1e780248f3c898d2d0d2aed
Nguyên Vũ <fish17tb@gmail.com>
2025-04-15 20:28:49 +0700

    Design-View-Detail-Page (Static-Code)

--- src/pages/client/book.tsx
+++ src/pages/client/book.tsx
@@ -1,22 +1,36 @@
 /* eslint-disable prefer-const */
 import BookDetail from "@/components/client/book/book.detail";
-import { useEffect } from "react";
+import { getBookByIdAPI } from "@/services/api";
+import { App } from "antd";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const BookPage = () => {
-    let { id } = useParams();
+  let { id } = useParams();
+  const { notification } = App.useApp();
+  const [currentBook, setCurrentBook] = useState<IBookTable | null>(null);
 
-    useEffect(() => {
-        if (id) {
-            //do something
-            console.log("book id = ", id)
+  useEffect(() => {
+    if (id) {
+      const fetchBookById = async () => {
+        const res = await getBookByIdAPI(id);
+        if (res && res.data) {
+          setCurrentBook(res.data);
+        } else {
+          notification.error({
+            message: "Đã có lỗi xảy ra",
+            description: res.message,
+          });
         }
-    }, [id])
-    return (
-        <div>
-            <BookDetail />
-        </div>
-    )
-}
+      };
+      fetchBookById();
+    }
+  }, [id]);
+  return (
+    <div>
+      <BookDetail currentBook={currentBook} />
+    </div>
+  );
+};
 
 export default BookPage;
